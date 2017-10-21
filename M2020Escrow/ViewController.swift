fe42e5f85f0b2883bf2a4b97dd892a9175271f7c
Nathan khosla <nathan@nkhosla.com>
2017-10-21 11:07:13 -0700

    Initial Commit

---
+++ M2020Escrow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  M2020Escrow
+//
+//  Created by Nathan khosla on 21 Oct 17.
+//  Copyright © 2017 Nathan khosla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ M2020EscrowTests/M2020EscrowTests.swift
@@ -0,0 +1,36 @@
+//
+//  M2020EscrowTests.swift
+//  M2020EscrowTests
+//
+//  Created by Nathan khosla on 21 Oct 17.
+//  Copyright © 2017 Nathan khosla. All rights reserved.
+//
+
+import XCTest
+@testable import M2020Escrow
+
+class M2020EscrowTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
