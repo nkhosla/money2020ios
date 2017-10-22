403596dd24da06ebc139692ba9fbd00068545a65
Nathan khosla <nathan@nkhosla.com>
2017-10-22 01:18:32 -0700

    lol commited to develop becuase I forgot to make a feature branch

--- M2020Escrow/ViewController.swift
+++ M2020Escrow/ViewController.swift
@@ -7,24 +7,26 @@
 //
 
 import UIKit
+import MaterialComponents
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         // Check if we should show the first launch flow
-
+        
         let hasLoggedInBefore = UserDefaults.standard.bool(forKey: "hasLoggedIn");
         
         if !hasLoggedInBefore {
+
             print("has not logged in before");
-  /**          let sb = UIStoryboard(name: "NewUserFlow", bundle: nil);
-            guard let vc = sb.instantiateInitialViewController() else {
-                print("the vc from first login flow storyboard dint load")
-                return;
-            }; **/
+            /**          let sb = UIStoryboard(name: "NewUserFlow", bundle: nil);
+             guard let vc = sb.instantiateInitialViewController() else {
+             print("the vc from first login flow storyboard dint load")
+             return;
+             }; **/
             
             let vc = SignInFirstStep(nibName: "SignInFirstStep", bundle: nil)
             vc.title = "Sign In"
@@ -32,14 +34,31 @@ class ViewController: UIViewController {
             
             self.present(nc, animated: true, completion: nil)
         }
-
+            
+        else {
+            
+            let floatingButton = MDCFloatingButton()
+            floatingButton.setTitle("+", for: .normal)
+            floatingButton.sizeToFit()
+            floatingButton.frame = CGRect(x:307,y:590,width:56,height:56)
+            floatingButton.addTarget(self, action: #selector(fabtapped), for: .touchUpInside)
+            self.view.addSubview(floatingButton)
+            
+        }
+        
     }
-
+    
+    @objc func fabtapped() {
+        print("fab tapped")
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
